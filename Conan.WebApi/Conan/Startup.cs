874049521fcf51072341c528e8087ccc389b282b
eldopacyna <cwiakowski.maciej@gmail.com>
2019-07-19 21:44:53 +0200

    Added Swagger

--- Conan.WebApi/Conan/Startup.cs
+++ Conan.WebApi/Conan/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Conan.Api
 {
@@ -22,14 +23,19 @@ namespace Conan.Api
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        { 
+        {
+            services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
             services.AddSignalR();
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection"),
                     b => b.MigrationsAssembly("Conan.Data")));
             services.AddScoped<UserManager<AppUser>>();
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -51,6 +57,12 @@ namespace Conan.Api
             app.UseCookiePolicy();
 
             //app.UseAuthentication();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = "";
+            });
             app.UseSignalR(route => { route.MapHub<ChatHub>("/chat"); });
         }
     }
