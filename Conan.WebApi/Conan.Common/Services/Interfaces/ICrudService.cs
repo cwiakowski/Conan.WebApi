4896bbab899ce1cbfaf955714a7b5057a0d5ad6c
eldopacyna <cwiakowski.maciej@gmail.com>
2019-07-27 14:05:29 +0200

    Small fixes and code refactorings

--- Conan.WebApi/Conan.Common/Services/Interfaces/ICrudService.cs
+++ Conan.WebApi/Conan.Common/Services/Interfaces/ICrudService.cs
@@ -7,8 +7,8 @@ namespace Conan.Common.Services.Interfaces
 {
     public interface ICrudService<T>
     {
-        Task<IEnumerable<MessageDTO>> Get();
-        Task<MessageDTO> Get(int id);
+        Task<IEnumerable<T>> Get();
+        Task<T> Get(int id);
         Task Insert(T item);
         Task<bool> Update(T item);
         Task<bool> Delete(int item);
--- Conan.WebApi/Conan.Data/Repositories/Interfaces/ICrudRepository.cs
+++ Conan.WebApi/Conan.Data/Repositories/Interfaces/ICrudRepository.cs
@@ -6,7 +6,7 @@ namespace Conan.Data.Repositories.Interfaces
 {
     public interface ICrudRepository<T>
     {
-        Task<IEnumerable<Message>> Get();
+        Task<IEnumerable<T>> Get();
         Task<T> Get(int id);
         Task Insert(T item);
         Task<bool> Update(T item);
