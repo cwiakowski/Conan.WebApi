e047c73e959b6e4cae1b99be479de44361a95147
eldopacyna <cwiakowski.maciej@gmail.com>
2019-07-28 15:21:55 +0200

    Fixes

--- Conan.WebApi/Conan.Common/DTO/MessageDTO.cs
+++ Conan.WebApi/Conan.Common/DTO/MessageDTO.cs
@@ -3,11 +3,12 @@
 namespace Conan.Common.DTO
 {
     public class MessageDTO
-    {
+    { 
         public int Id { get; set; }
-        public string Text { get; set; }
         public DateTime Date { get; set; }
-        public string SenderID { get; set; }
-        public string ReceiverID { get; set; }
+        public string FromUserId { get; set; }
+        public string ToUserId { get; set; }
+        public string Body { get; set; }
+
     }
 }
