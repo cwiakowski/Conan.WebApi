a932cc5f76bb37313fcd29ee12d3c2b543d12c80
eldopacyna <cwiakowski.maciej@gmail.com>
2019-10-03 13:06:57 +0200

    Added MessageHeaders

---
+++ Conan.WebApi/Conan.Common/DTO/MessageHeaderDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conan.Common.DTO
+{
+    public class MessageHeaderDTO
+    {
+        public int Id { get; set; }
+        public string LastMessage { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
--- Conan.WebApi/Conan/Controllers/AccountsController.cs
+++ Conan.WebApi/Conan/Controllers/AccountsController.cs
@@ -100,7 +100,7 @@ namespace Conan.Api.Controllers
             {
                 new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Name, user.UserName),
             };
 
